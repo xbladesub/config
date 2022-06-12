ba2193ba2f8e80f27f526b3ab60b21de6b83027b
Nikolai Shelekhov <nickshv13@icloud.com>
2022-06-12 19:56:28 +0800

    commit

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,3 +1,6 @@
+lvim.builtin.nvimtree.setup.view.preserve_window_proportions = true
+-- lvim.builtin.nvimtree.setup.actions.open_file.resize_window = true
+
 local settings = require("settings")
 settings.setOptions()
 settings.setKeymaps()
@@ -240,7 +243,7 @@ require("lvim.lsp.manager").setup("sourcekit", opts)
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "solc" })
 nvim_lsp['solc'].setup {
 	autostart = true,
-	cmd = { "/Users/nshv/Library/Python/3.8/bin/solc", "--lsp" },
+	cmd = { "/opt/homebrew/bin/solc", "--lsp" },
 	filetypes = { "solidity" },
 	-- rootdir = root_pattern(".git")
 }
