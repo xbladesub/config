13a16cc5424b9db61e4e4e9b97918bdee57f2a40
Nikolai Shelekhov <nickshv13@icloud.com>
2022-07-05 12:13:26 +0400

    commit

--- lvim/config.lua
+++ lvim/config.lua
@@ -367,8 +367,8 @@ vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
 
 require('onedark').setup {
 	-- Main options --
-	style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-	transparent = true, -- Show/hide background
+	style = 'darker', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	transparent = false, -- Show/hide background
 	term_colors = true, -- Change terminal color as per the selected theme style
 	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
 	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
@@ -769,7 +769,7 @@ else
 	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12"
 end
 
-vim.g.neovide_transparency = 0.75
+-- vim.g.neovide_transparency = 0.75
 vim.g.neovide_cursor_vfx_mode = "ripple"
 -- vim.g.neovide_remember_window_size = true
 -- vim.g.neovide_remember_window_position = true
